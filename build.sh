9dd77abbed585e3da164251cbbb879cc0ad1251f
Wouter Devinck <wouter.devinck@gmail.com>
2014-10-08 22:38:09 +0200

    Build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget http://buildroot.org/downloads/buildroot-2014.08.tar.gz
-tar -xzvf buildroot-2014.08.tar.gz buildroot
+tar -xzvf buildroot-2014.08.tar.gz -C buildroot
 rm buildroot-2014.08.tar.gz
 cd buildroot/
 make clonos_defconfig
