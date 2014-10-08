2f07192010244567419267de83b387ddcc158e06
root <root@vps103027.ovh.net>
2014-10-08 22:51:05 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 wget http://buildroot.org/downloads/buildroot-2014.08.tar.gz
-tar -xzvf buildroot-2014.08.tar.gz -C buildroot
+tar -xzvf buildroot-2014.08.tar.gz -C buildroot --strip-components 1
 rm buildroot-2014.08.tar.gz
 cd buildroot/
 make clonos_defconfig
+make
