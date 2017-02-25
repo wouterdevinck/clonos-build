1d1c9b0e833a75e931e2f6c7bdc755c0d13aa2eb
Wouter Devinck <wouter.devinck@gmail.com>
2017-02-25 23:45:18 +0100

    Buildroot version upgrade

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-wget http://buildroot.org/downloads/buildroot-2014.08.tar.gz
-tar -xzvf buildroot-2014.08.tar.gz -C buildroot --strip-components 1
-rm buildroot-2014.08.tar.gz
+BR_VERSION=buildroot-2017.02-rc2
+
+wget https://buildroot.org/downloads/$BR_VERSION.tar.gz -O buildroot.tar.gz
+tar -xzvf buildroot.tar.gz -C buildroot --strip-components 1
+rm buildroot.tar.gz
 cd buildroot/
 make clonos_defconfig
 make
