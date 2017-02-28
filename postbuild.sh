974fc56b13ced5bbe53d61508de26a97a63dfbd5
Wouter Devinck <wouter.devinck@gmail.com>
2017-02-28 22:50:09 +0100

    Image script

--- postbuild.sh
+++ postbuild.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-dd if=/dev/zero of=sdimage bs=1M count=64
+IMG_NAME=sdcard.img
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo "Creating image file"
+echo "==================="
+echo ""
+
+dd if=/dev/zero of=$IMG_NAME bs=1M count=64
+
+echo ""
+echo "Writing partition table"
+echo "======================="
+echo ""
+
 echo "n
 p
 1
@@ -14,5 +31,51 @@ p
 
 
 w
-" | fdisk sdimage
-losetup /dev/loop0 sdimage # TODO http://unix.stackexchange.com/questions/157876/creating-disk-device-in-a-file
+" | fdisk $IMG_NAME
+
+echo ""
+echo "Setting up loop device"
+echo "======================"
+echo ""
+
+losetup -o 34603008 /dev/loop0 $IMG_NAME
+
+echo ""
+echo "Creating ext4 file system"
+echo "========================="
+echo ""
+
+mkfs.ext4 -O ^metadata_csum,^64bit /dev/loop0
+
+echo ""
+echo "Mounting file system"
+echo "===================="
+echo ""
+
+mkdir mnt
+mount /dev/loop0 mnt
+
+echo ""
+echo "Copying files"
+echo "============="
+echo ""
+
+bsdtar -xpf buildroot/output/images/rootfs.tar -C mnt
+
+echo ""
+echo "Copying U-boot"
+echo "=============="
+echo ""
+
+dd if=buildroot/output/images/u-boot.sb of=/dev/loop0 bs=512 seek=4
+sync
+
+echo ""
+echo "Cleanup"
+echo "======="
+echo ""
+
+umount mnt
+sync
+losetup -d /dev/loop0
+rm -r mnt
